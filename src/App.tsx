98b85021bd45cd5873d987ac13789d4021f0bd7b
wookki <bwlimtony@gmail.com>
2023-11-22 16:27:25 +0900

    feat: react-router-dom 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<h2>안녕</h2>} />
+      <Route path='/posts' element={<h2>안녕</h2>} />
+      <Route path='/posts/:id' element={<h2>안녕</h2>} />
+      <Route path='/posts/new' element={<h2>안녕</h2>} />
+      <Route path='/posts/edit/:id' element={<h2>안녕</h2>} />
+      <Route path='/profile' element={<h2>안녕</h2>} />
+      <Route path='/profile/edit' element={<h2>안녕</h2>} />
+      <Route path='/notifications' element={<h2>안녕</h2>} />
+      <Route path='/search' element={<h2>안녕</h2>} />
+      <Route path='/users/login' element={<h2>안녕</h2>} />
+      <Route path='/users/signup' element={<h2>안녕</h2>} />
+      <Route path='*' element={<Navigate replace to='/' />} />
+    </Routes>
   );
 }
 
